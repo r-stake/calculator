efa07fb70fb49c4a87d15901b82150c7560a5839
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-17 10:54:14 +0300

    Create function that calls one of the operations
    Created an array to store functions handling the basic
    operations of the calculator. Created function "operate",
    which calls the correct math operation based on the provided
    operator.

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,17 @@
-function add(num1, num2) {
-    return num1 + num2;
+let num1 = 0;
+let num2 = 0;
+let operator = 0;
+
+const operations = [
+    { name: "+", operation: (num1, num2) => {return num1 + num2} },
+    { name: "-", operation: (num1, num2) => {return num1 - num2} },
+    { name: "*", operation: (num1, num2) => {return num1 * num2} },
+    { name: "/", operation: (num1, num2) => {return num1 / num2} }
+]
+
+function operate(operator, num1, num2) {
+    const currentOperation = operations.find(item => item.name === operator);
+    return currentOperation.operation(num1, num2);
 }
 
-function substract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
+console.log(operate("/", 10, 2));
