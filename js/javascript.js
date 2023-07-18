c4c627893255446c77e14faf9bfe4f9cff6dcbbd
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-18 16:51:12 +0300

    Add event listeners and a display screen function
    Created event listeners for digit and clear buttons. Created a function,
    which when combined with an event listener for digit buttons, populates
    the display screen with numbers.

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,13 @@
 let num1 = 0;
 let num2 = 0;
 let operator = 0;
+let displayString = 0;
+const displayCurrentScreen = document.querySelector(".current");
+const displayPreviousScreen = document.querySelector(".previous");
+const displayScreenOperator = document.querySelector(".display-operator");
+const btnDigits = document.querySelectorAll(".digit");
+const btnClear = document.querySelector(".clear");
+const btnOperators = document.querySelectorAll(".operator");
 
 const operations = [
     { name: "+", operation: (num1, num2) => {return num1 + num2} },
@@ -14,4 +21,37 @@ function operate(operator, num1, num2) {
     return currentOperation.operation(num1, num2);
 }
 
-console.log(operate("/", 10, 2));
+function displayDigits(input) {
+    if (displayCurrentScreen.textContent === "0") {
+        displayString = input;
+        displayCurrentScreen.textContent = displayString;
+    } else {
+        displayString += input;
+        displayCurrentScreen.textContent += input;
+    }
+    console.log(displayString);
+}
+
+btnDigits.forEach(digit => {
+    digit.addEventListener("click", function() {
+        displayDigits(this.textContent);
+    });
+});
+
+btnClear.addEventListener("click", () => {
+    displayCurrentScreen.textContent = "0"
+    displayPreviousScreen.textContent = ""
+});
+
+btnOperators.forEach(operator => {
+    operator.addEventListener("click", function() {
+        operator = this.textContent;
+        num1 = +displayString;
+        displayCurrentScreen.textContent = "0";
+        displayPreviousScreen.textContent = displayString;
+        displayScreenOperator.textContent = this.textContent;
+        displayPreviousScreen.appendChild(displayScreenOperator);
+    });
+});
+console.log(displayPreviousScreen);
+console.log(btnOperators);
