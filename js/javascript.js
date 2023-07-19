74e6fd23debb547e1612b3b325ff3c515a8e9dd3
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-19 14:14:30 +0300

    Complete calculation and result showing functionality

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,22 @@
 let num1 = 0;
 let num2 = 0;
-let operator = 0;
+let selectedOperator = "";
 let displayString = 0;
+let result = 0;
+
 const displayCurrentScreen = document.querySelector(".current");
 const displayPreviousScreen = document.querySelector(".previous");
 const displayScreenOperator = document.querySelector(".display-operator");
 const btnDigits = document.querySelectorAll(".digit");
 const btnClear = document.querySelector(".clear");
 const btnOperators = document.querySelectorAll(".operator");
+const btnEquals = document.querySelector(".equals");
 
 const operations = [
     { name: "+", operation: (num1, num2) => {return num1 + num2} },
     { name: "-", operation: (num1, num2) => {return num1 - num2} },
-    { name: "*", operation: (num1, num2) => {return num1 * num2} },
-    { name: "/", operation: (num1, num2) => {return num1 / num2} }
+    { name: "x", operation: (num1, num2) => {return num1 * num2} },
+    { name: "÷", operation: (num1, num2) => {return num1 / num2} }
 ]
 
 function operate(operator, num1, num2) {
@@ -45,13 +48,21 @@ btnClear.addEventListener("click", () => {
 
 btnOperators.forEach(operator => {
     operator.addEventListener("click", function() {
-        operator = this.textContent;
+        selectedOperator = this.textContent;
         num1 = +displayString;
-        displayCurrentScreen.textContent = "0";
         displayPreviousScreen.textContent = displayString;
+        displayString = "0";
+        displayCurrentScreen.textContent = "0";
         displayScreenOperator.textContent = this.textContent;
         displayPreviousScreen.appendChild(displayScreenOperator);
+        console.log(selectedOperator);
     });
 });
-console.log(displayPreviousScreen);
-console.log(btnOperators);
+
+btnEquals.addEventListener("click", function() {
+    num2 = +displayString;
+    displayPreviousScreen.textContent = "";
+    result = operate(selectedOperator, num1, num2);
+    displayString = result;
+    displayCurrentScreen.textContent = result;
+});
